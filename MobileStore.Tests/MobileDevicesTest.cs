ed45dd63d3d6bb7e4d8740904d540b9c48cfe61f
suresh-siddagari <suresh.csea5@gmail.com>
2018-12-04 15:11:27 +0100

    code refactoring and integration testing

--- MobileStore.Tests/MobileDevicesTest.cs
+++ MobileStore.Tests/MobileDevicesTest.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using MobileStore.Models;
+using NUnit.Framework;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 
 namespace MobileStore.Tests
 {
@@ -6,9 +11,12 @@ namespace MobileStore.Tests
     public class MobileDevicesTest
     {
 
+
+
         //Unit test for GetDiscountPrice()
+        //Here, we test only discount price logic only
         [Test]
-        public void MobileDevice_DiscountPrice_Test()
+        public void MobileDevice_Calculate_DiscountPrice_Test()
         {
             //Arrange
             MobileStore.Services.MobileDeviceService mobileService = new Services.MobileDeviceService();
@@ -18,7 +26,30 @@ namespace MobileStore.Tests
             double actualPrice = mobileService.GetDiscountPrice(7500);
 
             //Assert
-            Assert.That(actualPrice==expectedPrice,"Failed in calcuating discount price");
+            Assert.That(actualPrice == expectedPrice, "Failed in calcuating discount price");
         }
+
+        //Integration testing
+        //Here, we test the flow by getting price through API
+        [Test]
+        public void MobileDevice_GetDevicePrice_Test()
+        {
+
+            //Arrange
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            var client = new RestClient("https://mobilestoreapi.azurewebsites.net");
+            var request = new RestRequest("api/ver1/mobiledevices/1", Method.GET);
+            int expectedPrice = 4999;
+
+            //Act
+            // execute the request
+             MobileDevice mobileDevice = client.Execute<MobileDevice>(request).Data;
+
+            //Assert
+            Assert.AreEqual(expectedPrice, mobileDevice.Price);
+
+        }
+
+
     }
 }
