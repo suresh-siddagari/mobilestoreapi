2bd6725f38ce3c6a4db7c39f9c48fce28442145f
suresh-siddagari <suresh.csea5@gmail.com>
2018-12-04 13:08:38 +0100

    added UNIT tests

---
+++ MobileStore.Tests/MobileDevicesTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace MobileStore.Tests
+{
+    [TestFixture]
+    public class MobileDevicesTest
+    {
+
+        //Unit test for GetDiscountPrice()
+        [Test]
+        public void MobileDevice_DiscountPrice_Test()
+        {
+            //Arrange
+            MobileStore.Services.MobileDeviceService mobileService = new Services.MobileDeviceService();
+            double expectedPrice = 6850;
+
+            //Act
+            double actualPrice = mobileService.GetDiscountPrice(7500);
+
+            //Assert
+            Assert.That(actualPrice==expectedPrice,"Failed in calcuating discount price");
+        }
+    }
+}
